28b544a7bd0e1b2456c7e8c1784d28691969a075
Kieran0507 <k.goodinson94@gmail.com>
2021-10-05 12:29:12 +0100

    changed message

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const randName = nameList[Math.floor(Math.random() * nameList.length)];
 const randFood = foods[Math.floor(Math.random() * foods.length)];
 
 let websiteString = document.createElement('h2');
-websiteString.textContent = `${randName}'s favourite food is ${randFood}`;
+websiteString.textContent = `${randName}'s favourite meal is ${randFood}`;
 
 let websiteTitle = document.querySelector('#title');
 console.log(websiteTitle);
