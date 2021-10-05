0669cf13497c1005bd5d9213999448cf8ef2f19c
Kieran0507 <k.goodinson94@gmail.com>
2021-10-05 12:26:57 +0100

    added name and meal

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 
 let nameList = [
     "Reece",
-    "Tony"
+    "Tony",
+    "kieran"
 ];
 
 let foods = [
     "Pizza", 
-    "Lasagna"
+    "Lasagna",
+    "Steak"
 ];
 
 const randName = nameList[Math.floor(Math.random() * nameList.length)];
